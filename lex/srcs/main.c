96c2a18685527c5dc292dfb3627395055b60d54c
Morand <morand.etienne@gmail.com>
2025-06-27 15:55:12 +0700

    added file error management

--- lex/srcs/main.c
+++ lex/srcs/main.c
@@ -4,8 +4,9 @@ int main(int ac, char **av)
 {
     if (ac != 2)
     {
-        fprintf(stderr, "Usage: %s <input>\n", av[0]);
+        fprintf(stderr, "Usage: %s <file.l>\n", av[0]);
         return 1;
     }
+    file_error(av[1]);
     return 0;
 }
