580a97c8463f7ea57e508888c33ca051d4822841
Morand <morand.etienne@gmail.com>
2025-07-07 14:45:22 +0700

    setup basic function for macros

--- lex/includes/lex.h
+++ lex/includes/lex.h
@@ -5,11 +5,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include "parser.h"
+#include "first_section/macros.h"
 
 typedef struct s_lex
 {
     // 1 st section of the lexer file
     char    *declaration_code; // string that will contain the declarations between %{ and %}
+    t_macros_list macros; // list of macros
     // 2 nd section of the lexer file
     t_parser parser;
 }   t_lex;
